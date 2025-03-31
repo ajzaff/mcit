e9f86d9ba37f15ec0b0d3854abfa9a0722daae5c
ajzaff <4390902+ajzaff@users.noreply.github.com>
2025-03-31 00:07:23 -0400

    feat(plugin): Add Go plugin helper package

---
+++ plugin/plugin.go
@@ -0,0 +1,28 @@
+package plugin
+
+import (
+	"fmt"
+	"plugin"
+
+	"github.com/ajzaff/mcit"
+)
+
+// LoadSearchFunc loads a search function named "Search" from the plugin source.
+//
+// Compile a plugin using the `go build -buildmode=plugin` feature of Go
+// (https://pkg.go.dev/plugin.) Only supported on Linux, FreeBSD, and Mac.
+func LoadSearchFunc(path string) (mcit.Func, error) {
+	x, err := plugin.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	sym, err := x.Lookup("Search")
+	if err != nil {
+		return nil, err
+	}
+	fn, ok := sym.(mcit.Func)
+	if !ok {
+		return nil, fmt.Errorf("expected Search to be %T, but found %T", mcit.Func(nil), fn)
+	}
+	return fn, nil
+}
